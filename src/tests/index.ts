31fd1430a5a4f525b5d49897335d4c35484a37d6
Silvestr Baibuz <silvestr-b@yandex.ru>
2017-12-18 17:06:19 +0300

    add tests for .deps method

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -104,6 +104,86 @@ describe('DIContainer', () => {
 
    })
 
+   describe('.deps', () => {
+
+      it('When dependencies for module is declared, should resolve and pass to resolver deps by name', done => {
+         const spy = sinon.spy(deps => new Sheep(deps.ISheepName))
+
+         container.register(TYPES.ISomeString)
+            .resolver(deps => 'FakeString')
+
+         container.register(TYPES.ISheepName)
+            .resolver(deps => 'FakeSheepName')
+
+         container.register(TYPES.ISheep)
+            .deps(TYPES.ISheepName, TYPES.ISomeString)
+            .resolver(spy)
+
+         container.get(TYPES.ISheep)
+            .then(result => expect(spy.firstCall.args[0]).to.be.eql({ ISheepName: 'FakeSheepName', ISomeString: 'FakeString' }))
+            .then(() => done())
+      })
+
+      it('When dependencies have own dependencies, should recursively resolve all dependency', done => {
+         container.register(TYPES.ISheepName)
+            .resolver(deps => 'FakeSheepName')
+
+         container.register(TYPES.ISheep)
+            .deps(TYPES.ISheepName)
+            .resolver(deps => new Sheep(deps.ISheepName))
+
+         container.register(TYPES.ICat)
+            .deps(TYPES.ISheep)
+            .resolver(deps => new Cat(deps.ISheep))
+
+         container.register(TYPES.IDog)
+            .deps(TYPES.ICat, TYPES.ISheep)
+            .resolver(deps => new Dog(deps.ICat, deps.ISheep))
+
+         container.get(TYPES.IDog)
+            .then(result => {
+               expect(result).to.be.instanceOf(Dog)
+               expect(result.sheep).to.be.instanceOf(Sheep)
+               expect(result.sheep.name).to.be.string
+               expect(result.cat).to.be.instanceOf(Cat)
+               expect(result.cat.sheep).to.be.instanceOf(Sheep)
+               expect(result.cat.sheep.name).to.be.string
+            })
+            .then(() => done())
+      })
+
+      it('When dependency is not declared, should be rejected with exeption', done => {
+         container.register(TYPES.ISheepName)
+            .deps(<any>'NotExistedDependency')
+            .resolver(deps => 'FakeSheepName')
+
+         container.get(TYPES.ISheepName)
+            .then(null, err => {
+               expect(err.message).to.be.include('NotExistedDependency')
+            })
+            .then(() => done())
+      })
+
+      it('When exeption is throwed somewhere in dependencies chain, should be rejected with that exeption', done => {
+         container.register(TYPES.ISheepName)
+            .require('Name', () => { throw 'FakeMessage' })
+            .resolver(deps => 'FakeSheepName')
+
+         container.register(TYPES.ISheep)
+            .deps(TYPES.ISheepName)
+            .resolver(deps => new Sheep(deps.ISheepName))
+
+         container.register(TYPES.ICat)
+            .deps(TYPES.ISheep)
+            .resolver(deps => new Cat(deps.ISheep))
+
+         container.get(TYPES.ICat)
+            .then(null, err => expect(err).to.be.equal('FakeMessage'))
+            .then(() => done())
+      })
+
+   })
+
 })
 
 
