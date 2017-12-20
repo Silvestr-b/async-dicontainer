e3d75f84ac014f34ab0fcfad09c3792cc3a65166
Silvestr Baibuz <silvestr-b@yandex.ru>
2017-12-20 21:26:57 +0300

    add test for multiple declared whenParent callbacks

--- src/tests/whenParent.ts
+++ src/tests/whenParent.ts
@@ -20,6 +20,85 @@ describe('.whenParent', () => {
       notCallableSpy = sinon.spy();
    })
 
+   it('When returns true, declaration should be resolved', done => {
+      container.register(TYPES.ISheepName).resolver(deps => 'DefaultSheepName')
+
+      container.register(TYPES.ISheepName)
+         .whenParent(() => true)
+         .resolver(deps => 'SecondDeclaration')
+
+      container.register(TYPES.ISheep)
+         .deps(TYPES.ISheepName)
+         .resolver(deps => new Sheep(deps.ISheepName))
+
+      container.get(TYPES.ISheep)
+         .then(spy, notCallableSpy)
+         .then(() => {
+            expect(spy.firstCall.args[0].name).to.be.equal('SecondDeclaration')
+            expect(notCallableSpy.notCalled).to.be.true
+         })
+         .then(() => done())
+   })
+
+   it('When returns false, another declaration should be resolved', done => {
+      container.register(TYPES.ISheepName).resolver(deps => 'DefaultSheepName')
+
+      container.register(TYPES.ISheepName)
+         .whenParent(() => false)
+         .resolver(deps => 'SecondDeclaration')
+
+      container.get(TYPES.ISheepName)
+         .then(spy, notCallableSpy)
+         .then(() => {
+            expect(spy.firstCall.args[0]).to.be.equal('DefaultSheepName')
+            expect(notCallableSpy.notCalled).to.be.true
+         })
+         .then(() => done())
+   })
+
+   it('When declared several "whenParent" callbacks, should execute all', done => {
+      const whenSpy1 = sinon.spy(() => true);
+      const whenSpy2 = sinon.spy(() => true);
+
+      container.register(TYPES.ISheepName).resolver(deps => 'DefaultSheepName')
+
+      container.register(TYPES.ISheepName)
+         .whenParent(whenSpy1)
+         .whenParent(whenSpy2)
+         .resolver(deps => 'SecondDeclaration')
+
+      container.register(TYPES.ISheep)
+         .deps(TYPES.ISheepName)
+         .resolver(deps => new Sheep(deps.ISheepName))
+
+      container.get(TYPES.ISheep)
+         .then(spy, notCallableSpy)
+         .then(() => {
+            expect(spy.firstCall.args[0].name).to.be.equal('SecondDeclaration');
+            expect(whenSpy1.called).to.be.true;
+            expect(whenSpy2.called).to.be.true;
+            expect(notCallableSpy.notCalled).to.be.true;
+         })
+         .then(() => done())
+   })
+
+   it('When one of several "whenParent" callbacks return false, another declaration should be resolved', done => {
+      container.register(TYPES.ISheepName).resolver(deps => 'DefaultSheepName')
+
+      container.register(TYPES.ISheepName)
+         .whenParent(() => false)
+         .whenParent(() => true)
+         .resolver(deps => 'SecondDeclaration')
+
+      container.get(TYPES.ISheepName)
+         .then(spy, notCallableSpy)
+         .then(() => {
+            expect(spy.firstCall.args[0]).to.be.equal('DefaultSheepName');
+            expect(notCallableSpy.notCalled).to.be.true;
+         })
+         .then(() => done())
+   })
+
    it('When parent is existed, argument should be parent context object', done => {
       const spy = sinon.spy(() => true)
       const parentSpy = sinon.spy(() => true)
