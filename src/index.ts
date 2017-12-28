c98a3c84725f05cf007e46b334a604d5e4a11cf1
Silvestr Baibuz <silvestr-b@yandex.ru>
2017-12-28 20:59:34 +0300

    rename entities directory to main

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { Container } from './entities/Container'
+export { Container } from './main/Container'
 export { RequiredDeps } from './interfaces/RequiredDeps'
 export { ResolvedDeps } from './interfaces/ResolvedDeps'
 export { DataFetchers } from './interfaces/DataFetchers'
